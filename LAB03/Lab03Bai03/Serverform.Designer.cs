aad9cc6e47924a0650aeefc24ff0156c11aca5cc
Trần Kim Ngân (Kylna) <24521135@gm.uit.edu.vn>
2025-11-15 20:03:15 +0700

    [Kngan] Up lai lab03 Bai3,4 (#8)

---
+++ LAB03/Lab03Bai03/Serverform.Designer.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Lab03Bai03
+{
+    partial class ServerForm
+    {
+        private IContainer components = null;
+        private Button btnListen;
+        private TextBox txtLog;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnListen = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(12, 12);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(90, 30);
+            this.btnListen.TabIndex = 0;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 55);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(460, 280);
+            this.txtLog.TabIndex = 1;
+            // 
+            // ServerForm
+            // 
+            this.ClientSize = new System.Drawing.Size(484, 351);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnListen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ LAB03/Lab03Bai04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab03
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+          
+            Application.Run(new FrmDashboard());
+        }
+    }
+}
